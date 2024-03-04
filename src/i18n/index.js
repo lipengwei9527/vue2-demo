73c7639d96f78bc360e17aba617ea2f3cb5a7440
lipengwei <1970586140@qq.com>
2024-03-04 16:20:59 +0800

    语言切换修改

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,33 +1,38 @@
 import Vue from "vue"
 import VueI18n from "vue-i18n"
 import ElementLocal from 'element-ui/lib/locale'
-import enLocale from 'element-ui/lib/locale/lang/en' //引入elementui语言包
-import zhLocale from 'element-ui/lib/locale/lang/zh-CN' //引入ele语言包
+import enElement from 'element-ui/lib/locale/lang/en' //引入elementui语言包
+import zhElement from 'element-ui/lib/locale/lang/zh-CN' //引入ele语言包
+
+// 本地语言
+import zhLocal from './lang/zh'
+import enLocal from './lang/en'
+
 Vue.use(VueI18n)
-function loadLocalMessages() {
-  const locals = require.context('.', true, /.json$/i)
-  const messages = {}
-  locals.keys().forEach(key => {
-    messages[key.replace('./', '').replace('.json', '')] = locals(key)
-  })
-  return messages
-}
-// 加载的json语言包
-const localMessages = loadLocalMessages()
+// function loadLocalMessages() {
+//   const locals = require.context('.', true, /.json$/i)
+//   const messages = {}
+//   locals.keys().forEach(key => {
+//     messages[key.replace('./', '').replace('.json', '')] = locals(key)
+//   })
+//   return messages
+// }
+// // 加载的json语言包
+// const localMessages = loadLocalMessages()
+
 
 const i18n = new VueI18n({
   locale: localStorage.getItem('local') || 'zh',//页面当前使用的语言的标识
   messages: {
-    ...localMessages,
     zh: {
-      ...zhLocale, ...localMessages.zh
+      ...zhElement, ...zhLocal
     },
     en: {
-      ...enLocale, ...localMessages.en
+      ...enElement, ...enLocal
     },
 
   },
-  // silentTranslationWarn: true
+  silentTranslationWarn: true
 })
 
 ElementLocal.i18n((key, value) => i18n.t(key, value))
